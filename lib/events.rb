aec48349b6c2c5e43011bedd489a917c77ac12c0
Bill Nelson <bill@olsonrealestategroup.com>
2021-01-25 14:28:12 -0800

    Converted to module

--- lib/events.rb
+++ lib/events.rb
@@ -1,43 +1,46 @@
-module EVENTS
-    GENERAL='General Inquiry'
-  
-    def create_general_event_data(lead_source, lead_system, message, first_name, last_name, emails, phones)
-      parsed_emails = []
-      parsed_phones = []
-  
-      if emails.kind_of? String
-        parsed_emails.push("value": emails)
-      elsif emails.kind_of? Array
-        emails.each do |email|
-          parsed_emails.push("value": email)
-        end
-      end
+module FUB
 
-      if phones.kind_of? String
-        parsed_phones.push("value": phones)
-      elsif phones.kind_of? Array
-        phones.each do |phone|
-          parsed_phones.push("value": phone)
-        end
+  module INQUIRY
+    GENERAL='General Inquiry'
+  end
+
+  def create_general_event_data(lead_source, lead_system, message, first_name, last_name, emails, phones)
+    parsed_emails = []
+    parsed_phones = []
+
+    if emails.kind_of? String
+      parsed_emails.push("value": emails)
+    elsif emails.kind_of? Array
+      emails.each do |email|
+        parsed_emails.push("value": email)
       end
-  
-      person = {
-        "firstName": first_name,
-        "lastName": last_name,
-        "email": parsed_emails,
-        "phones": parsed_phones,
-      }
-  
-      data = {
-        "source": lead_source,
-        "system": lead_system,
-        "type": GENERAL,
-        "message": message,
-        "person": person
-      }
-  
-      return data
     end
 
-    module_function :create_general_event_data
+    if phones.kind_of? String
+      parsed_phones.push("value": phones)
+    elsif phones.kind_of? Array
+      phones.each do |phone|
+        parsed_phones.push("value": phone)
+      end
+    end
+
+    person = {
+      "firstName": first_name,
+      "lastName": last_name,
+      "email": parsed_emails,
+      "phones": parsed_phones,
+    }
+
+    data = {
+      "source": lead_source,
+      "system": lead_system,
+      "type": INQUIRY::GENERAL,
+      "message": message,
+      "person": person
+    }
+
+    return data
   end
+
+  module_function :create_general_event_data
+end
